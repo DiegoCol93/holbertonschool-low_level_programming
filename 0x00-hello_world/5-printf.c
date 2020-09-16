9708cf80a99defcf1b18586309945b7ef4f478df
Diego Lopez <dfelo93@gmail.com>
2020-09-16 20:35:28 +0000

    Added 5printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints message
+ * Return: 0, on completion.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
