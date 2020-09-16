ef1fefd478cce1512acb2f2694d658ef5345290b
Diego Lopez <dfelo93@gmail.com>
2020-09-16 20:17:35 +0000

    Proper betty format again on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Prints str using puts
+ * Description: Create char variable str
+ * Outputs to stdout using puts then return
+ * 0 when program ends.
+ * Return: 0 when program ends.
+ */
 int main(void)
 {
 char str[] = "\"Programming is like building a multilingual puzzle";
