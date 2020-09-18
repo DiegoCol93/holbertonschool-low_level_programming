e79aae8b4e752314af83f58b7ae8b6368c803308
Diego Lopez <dfelo93@gmail.com>
2020-09-18 04:54:40 +0000

    fix0 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,47 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - Prints 00 00 to 99 99 no repeating digits.
- *
- * Return: 0 Always, on completion.
+ * main - entry point
+ * Desc: Use `putchar` function to print number from 0001 to 9999
+ * Return: 0
  */
 int main(void)
 {
-int num0 = 48;
-int num1;
-int num2;
-int num3;
+	int i, j;
+	int q, w, e, r;
 
-while (num0 <= 57)
-{
-for (num1 = 48; num1 <= 57; num1++)
-{
-for (num2 = 48; num2 <= 57; num2++)
-{
-for (num3 = 48; num3 <= 57; num3++)
-{
-if (num1 < num3 && num0 <= num2)
-{
-putchar(num0);
-putchar(num1);
-putchar(32);
-putchar(num2);
-putchar(num3);
-if (num0 != 54 || num1 != 55 || num2 != 56 || num3 != 57)
-{
-if ((num0 + num1 + num2 + num3) == 227)
-{
-putchar(10);
-break;
-}
-putchar(44);
-putchar(32);
-}
-}
-}
-}
-}
-num0++;
-}
-return (0);
+	for (i = 0; i < 100; i++)
+	{
+		q = i / 10;
+		w = i % 10;
+
+		for (j = 0; j < 100; j++)
+		{
+			e = j / 10;
+			r = j % 10;
+
+			if (q < e || (q == e && w < r))
+			{
+				putchar(q + '0');
+				putchar(w + '0');
+				putchar(32);
+				putchar(e + '0');
+				putchar(r + '0');
+
+				if (!(q == 9 && w == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
