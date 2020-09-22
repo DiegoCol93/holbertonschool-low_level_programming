2ec9767214bdc0dfc6f2bc56084b4d5a6b666aa6
Diego Lopez <dfelo93@gmail.com>
2020-09-22 01:25:33 -0500

    Added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alphabet from a to z ten times.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int ch = 97;
+	int i;
+
+	for (i = 0 ; i <= 10; i++)
+	{
+		while (ch <= 122)
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar(10);
+		ch = 97;
+	}
+}
