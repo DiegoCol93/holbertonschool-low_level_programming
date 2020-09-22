a44e956fb6df83daa408855ff2fc4093acd8daf3
Diego Lopez <dfelo93@gmail.com>
2020-09-22 01:01:49 -0500

    Added holberton.h

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <ctype.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet - Prints alphabet from a to z
+ */
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - Prints alphabet from a to z ten times
+ */
+void print_alphabet_x10(void);
+
+/**
+ * _islower - Checks if c is an upper or lower case character
+ */
+int _islower(int c);
+
+/**
+ * _isalpha - Chekcs if c is an alphabetic character or not
+ */
+int _isalpha(int c);
