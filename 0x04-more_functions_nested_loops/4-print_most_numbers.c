daa5e5ecdfc435f213f8687328ffd403dbf5d258
Diego Lopez <dfelo93@gmail.com>
2020-09-24 12:51:36 -0500

    Added 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Prints 0 to 9, but 2 and 4.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+		}
+		_putchar(i);
+	}
+	_putchar(10);
+}
