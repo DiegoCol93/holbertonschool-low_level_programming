216a423ba8ca585ea74b0950ca964f9a4dc866b5
Diego Lopez <dfelo93@gmail.com>
2020-09-24 14:00:17 -0500

    Added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Prints Fizz for factors of 3, prints Buzz for factors of 5
+ *             and prints FizzBuzz for factors of 3 and 5, up to 100.
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	putchar(10);
+}
