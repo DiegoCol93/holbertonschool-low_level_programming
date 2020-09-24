d7ae71a02b469d468bb84579c3e271323471667b
Diego Lopez <dfelo93@gmail.com>
2020-09-24 12:47:08 -0500

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - Multiplies a and b together.
+ * @a: Number 1.
+ * @b: Number 2
+ *
+ * Return: Value of result.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
