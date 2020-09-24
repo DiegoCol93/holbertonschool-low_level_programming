d23586acfc12182f19c7218ba8554e0ae60df1c4
Diego Lopez <dfelo93@gmail.com>
2020-09-24 12:37:49 -0500

    Added holberton.h library

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,32 @@
+/* _putchar - Prints one character. */
+int _putchar(char c);
+
+/* _isupper - Checks if character is upper or lowercase. */
+int _isupper(int c);
+
+/* _isdigit - Checks if character is 0 through 9. */
+int _isdigit(int c);
+
+/* mul - Multiplies a and b together. */
+int mul(int a, int b);
+
+/* print_numbers - Prints characters 0 to 9. */
+void print_numbers(void);
+
+/* print_most_numbers - Prints 0 to 9, but 2 and 4. */
+void print_most_numbers(void);
+
+/* more_numbers - Prints 0 to 14 ten times. */
+void more_numbers(void);
+
+/* print_line - Draws a line in the terminal. */
+void print_line(int n);
+
+/* print_diagonal - Prints diagonal size n using "\". */
+void print_diagonal(int n);
+
+/* print_square - Prints square size n using "#". */
+void print_square(int size);
+
+/* print_triangle - Prints Triangle size n using "#". */
+void print_triangle(int size);
