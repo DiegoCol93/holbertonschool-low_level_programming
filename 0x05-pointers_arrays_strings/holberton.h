b364fd3c5a4f7ad5edbd25f5a9901b5aff338e1a
Diego Lopez <dfelo93@gmail.com>
2020-09-29 00:14:31 -0500

    Updated holberton.h

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,3 +3,18 @@ int _putchar(char c);
 
 /* reset_to_98 - Updates the value to 98 of the pointed space in memory. */
 void reset_to_98(int *n);
+
+/* swap_int - Swaps the value of a and b. */
+void swap_int(int *a, int *b);
+
+/* _strlen - Returns the leng of a string. */
+int _strlen(char *s);
+
+/* _puts - Prints a string. */
+void _puts(char *str);
+
+/* print_rev - Prints a string reversed. */
+void print_rev(char *s);
+
+/* rev_string - Reverts a string. */
+void rev_string(char *s);
