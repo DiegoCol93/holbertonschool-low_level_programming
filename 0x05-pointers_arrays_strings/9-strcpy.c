9cfd98cc78867f31b3423851af55e9695f55ec14
Diego Lopez <dfelo93@gmail.com>
2020-09-29 19:39:36 -0500

    Fix0 Betty 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 /**
  * *_strcpy - Copies a string.
+ * @dest: Destiny pointer to start writing from.
+ * @src: Source string pointer.
+ *
+ * Return: Returns dest pointer.
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -9,9 +13,9 @@ char *_strcpy(char *dest, char *src)
 	for (Lenght = 0; src[Lenght] != 0; Lenght++)
 	{
 	}
-	for(Write = 0; Write <= Lenght; Write++)
+	for (Write = 0; Write <= Lenght; Write++)
 	{
 		dest[Write] = src[Write];
 	}
-	return(dest);
+	return (dest);
 }
