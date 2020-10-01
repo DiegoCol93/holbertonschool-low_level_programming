4dffd07fb018fc092f5a66d2485581368e55c3b2
Diego Lopez <dfelo93@gmail.com>
2020-10-01 13:25:45 -0500

    Updated holberton.h up to 02

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,2 +1,11 @@
 /* _putchar - writes the character c to stdout */
 int _putchar(char c);
+
+/* _strcat - concatenates two strings. */
+char *_strcat(char *dest, char *src);
+
+/* char _strncat - Concatenates two strings up to index n of source. */
+char *_strncat(char *dest, char *src, int n);
+
+/* _strncpy - copy a string */
+char *_strncpy(char *dest, char *src, int n);
