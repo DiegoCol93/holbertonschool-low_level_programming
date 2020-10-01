3d5fe400c9ed1fae144834c0a342a40d518e30cc
Diego Lopez <dfelo93@gmail.com>
2020-10-01 16:51:57 -0500

    Added 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+char *cap_string(char *s)
+{
+	char NewWord[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int Write, Compare;
+
+	for (Write = 0; s[Write] != '\0'; Write++)
+	{
+		if (s[Write] >= 97 && s[Write] <= 122)
+		{
+			Compare = 0;
+			for (Compare = 0; Compare < 13; Compare++)
+			{
+				if (s[Write - 1] == NewWord[Compare])
+				{
+				s[Write] = s[Write] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
