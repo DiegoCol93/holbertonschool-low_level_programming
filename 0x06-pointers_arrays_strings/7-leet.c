ba8644bf835300f71b7f8f560ed6e604f9efb80a
Diego Lopez <dfelo93@gmail.com>
2020-10-01 22:15:10 -0500

    Fix0 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,15 @@
 char *leet(char *s)
 {
 	int Read, Write;
-	char Code[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7'};
-	char Letter[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	char Code[] = {"430714307"};
+	char Letter[] = {"aeotlAEOTL"};
 
 	for (Read = 0; s[Read] != '\0'; Read++)
 	{
-		for (Write = 0; Write <= 10; Write++)
+		for (Write = 0; Write < 10; Write++)
 		{
 			if (s[Read] == Letter[Write])
-			{
 				s[Read] = Code[Write];
-			}
 		}
 	}
 	return (s);
