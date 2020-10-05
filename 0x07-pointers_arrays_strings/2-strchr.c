d7738091a0e1b986333aef81f7f587dd583bcc2c
Diego Lopez <dfelo93@gmail.com>
2020-10-05 12:09:17 -0500

    Added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strchr - Locates a character in a string.
+ * @s: Target string.
+ * @c: Character to locate.
+ *
+ * Return: ptr, Pointer to first character ocurrence, NULL if c not found.
+ */
+char *_strchr(char *s, char c)
+{
+	char *ptr = NULL; /* <<- - - - - - - - - - - -Declaring NULL pointer. */
+	int i = 0; /* <<- - - - - - - - - - - - - - - - -Declaring counter i. */
+
+	while (s[i] != 0) /* <<- - - - - - - Runs through lenght of string s. */
+	{
+		if (s[i] == c) /* Comparing if character on index equal to c. */
+		{
+			ptr = &s[i]; /* Sets ptr to address of 1st char found */
+			return (ptr); /* <<- - -Return pointer to found char. */
+		}
+		i++; /* <<- - - - - - - - - - - - - -Move index to next step. */
+	}
+	return (ptr); /* <<- - - - - - - - - - -Return pointer to found char. */
+}
