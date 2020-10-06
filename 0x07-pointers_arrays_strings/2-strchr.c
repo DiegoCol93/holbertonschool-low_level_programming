14e80dd6df935400ed502ff4880c425388bf3543
Diego Lopez <dfelo93@gmail.com>
2020-10-05 19:21:23 -0500

    Fix12 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,16 +11,12 @@ char *_strchr(char *s, char c)
 	char *ptr = NULL; /* <<- - - - - - - - - - - -Declaring NULL pointer. */
 	int i = 0; /* <<- - - - - - - - - - - - - - - - -Declaring counter i. */
 
-	while (s[i] != 0) /* <<- - - - - - - Runs through lenght of string s. */
+	while (s[i]) /* <<- - - - - - - - - -Runs through lenght of string s. */
 	{
 		if (s[i] == c) /* Comparing if character on index equal to c. */
 		{
 			ptr = &s[i]; /* Sets ptr to address of 1st char found */
 			return (ptr); /* <<- - -Return pointer to found char. */
-		}
-		else if (s[i] == '\0')
-		{
-			return ('\0'); /* <<- - - Return if comparison ended. */		
 		}
 		i++; /* <<- - - - - - - - - - - - - -Move index to next step. */
 	}
