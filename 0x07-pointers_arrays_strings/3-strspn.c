eed39e78ae5e8e8ddf4ebab08d52ae99d54b6fe4
Diego Lopez <dfelo93@gmail.com>
2020-10-05 19:24:26 -0500

    Added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Target string.
+ * @accept: Prefixed string to search for.
+ *
+ * Return: n, Number of bytes from accept showing on s.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int len_S = 0; /* <<- - - - Declaring counter for lenght of string s. */
+	int len_Ac = 0; /* <<- Declaring counter for lenght of string accept. */
+	int comp = 0; /* <<- - - - - - - - - - -Declaring comparison counter. */
+	unsigned int times = 0; /* <<- - - Declaring counter for times found. */
+
+	for (; accept[len_Ac] != 0; len_Ac++)
+	{/* ^^ - - - - - - - - - - - - - - - - - - -Calculates prefix lenght. */
+	}
+	while (s[len_S] != 32) /* <<- - - -Runs through string up to a space. */
+	{
+		comp = 0; /* <<- - - - - - - - - - Resets comparison counter. */
+		while (comp < len_Ac) /* <<- - - - - - - - - Comparison loop. */
+		{
+			if (s[len_S] == accept[comp]) /* Comparing statement. */
+			{
+				times++; /* <<- - - Times character appeared. */
+				break; /* <<- - - Jumps to not repeat a char. */
+			}
+			comp++; /* <<- - - - - - - -Comparison plus one step. */
+		}
+		len_S++; /* <<- - - - - - - - - - Run to next step on string. */
+	}
+	return (times); /* <<- - - Return times characters appeared b4 space. */
+}
