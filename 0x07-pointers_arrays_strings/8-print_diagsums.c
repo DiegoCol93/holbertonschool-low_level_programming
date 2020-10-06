dc8efd5fda8af00a2a43100c95e6521b69261310
Diego Lopez <dfelo93@gmail.com>
2020-10-05 23:58:28 -0500

    Fix0 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,7 +11,7 @@ void print_diagsums(int *a, int size)
 	int i;
 	int sum1, sum2;
 
-	for (i = 0; i < (size * size); i += (size + 1))
+	for (i = 0; i <= (size * size); i += (size + 1))
 	{
 		sum1 += a[i];
 	}
