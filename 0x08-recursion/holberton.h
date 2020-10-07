9c9438c46dea9d296a7cc47fede2d759b6e8dd1b
Diego Lopez <dfelo93@gmail.com>
2020-10-07 12:28:26 -0500

    Added holberton.h

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,26 @@
+/* _putchar - writes the character c to stdout */
+int _putchar(char c);
+
+/* 0. _puts_recursion - prints a string, followed by a new line. */
+void _puts_recursion(char *s);
+
+/* 1. _print_rev_recursion - prints a string in reverse. */
+void _print_rev_recursion(char *s);
+
+/* 2. _strlen_recursion - returns the length of a string. */
+int _strlen_recursion(char *s);
+
+/* 3. factorial - returns the factorial of a given number. */
+int factorial(int n);
+
+/* 4. _pow_recursion - returns the value of x raised to the power of y. */
+int _pow_recursion(int x, int y);
+
+/* 5. _sqrt_recursion - returns the natural square root of a number. */
+int _sqrt_recursion(int n);
+
+/* 6. is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0. */
+int is_prime_number(int n);
+
+/* 7. is_palindrome - returns 1 if a string is a palindrome and 0 if not. */
+int is_palindrome(char *s);
