b1e6d77c82253e23f4a23b14c239a7b24c113957
Diego Lopez <dfelo93@gmail.com>
2020-10-10 12:22:50 -0500

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of the program.
+ * @argc: Unused variable.
+ * @argv: Array of arguments passed to the program.
+ *
+ * Return: 0 Always, on success.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
