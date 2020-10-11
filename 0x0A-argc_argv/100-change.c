f4033e0d955b6eb6595bde5a90a3a9b83fb47ce4
Diego Lopez <dfelo93@gmail.com>
2020-10-11 14:49:23 -0500

    Reset 11 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "holberton.h"
 /**
  * main - Prints the minimum number of coins to make change.
- *
  * @argc: Input number.
  * @argv: Input string.
  *
@@ -9,26 +11,25 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned long int i = 0, num = 0;
+	int num = 0, cents = 0, i;
 	int centsA[5] = {25, 10, 5, 2, 1};
-	unsigned long int cents = 0;
-	unsigned long int times[5] = {0};
+	int times[5] = {0};
 
 	if (argc == 1)
 	{
-		printf("Error\n");
-		return (1);
-	}
-	if (argv[1][0] == '-')
-	{
-		printf("0\n");
+		printf("\033[91mError\n\033[0m");
 		return (0);
 	}
 	num = atoi(argv[1]);
 	if (argc > 2)
 	{
-		printf("Error\n");
+		printf("\033[91mError\n\033[0m");
 		return (1);
+	}
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
 	}
 	for (i = 0; i < 5; i++)
 	{
@@ -36,6 +37,6 @@ int main(int argc, char *argv[])
 		num = num % centsA[i];
 	}
 	cents = times[0] + times[1] + times[2] + times[3] + times[4];
-	printf("%lu\n", cents);
+	printf("%d\n", cents);
 	return (0);
 }
