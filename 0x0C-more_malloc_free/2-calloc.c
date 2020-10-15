45123fda82da6b213f77613493bad207fff02dc8
Diego Lopez <dfelo93@gmail.com>
2020-10-15 15:58:31 -0500

    Added if when nmemb and size == 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,11 +13,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *array;
 	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
 	array = malloc(nmemb * size);
 	if (array)
+	{
 		for (i = 0; i < (nmemb * size); i++)
 		{
 			array[i] = 0;
 		}
+	}
 	return (array);
 }
