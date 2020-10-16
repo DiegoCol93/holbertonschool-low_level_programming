b5c442a4d4e60cfeebbc6f1217601646df57a3cb
Diego Lopez <dfelo93@gmail.com>
2020-10-15 20:51:28 -0500

    Reallocates a block of memory with a specific size.

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *  _realloc   -  Reallocates a block of memory with a specific size.
+ *
+ *    @ptr:    -  Target to previously allocated block of memory.
+ *  @old_size: -  Old size of type.
+ *  @new_size: -  New size of type.
+ *
+ * Return:     -  nptr, pointer to new block of memory, NULL if fails; ptr if
+ *                old_size == new_size.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *nptr = NULL, *optr = NULL;
+	unsigned int i;
+
+	if (old_size == new_size)
+		return (ptr);
+	optr = (char*)ptr;
+	nptr = malloc(new_size);
+	if (nptr)
+		for (i = 0; i < old_size; i++)
+			nptr[i] = optr[i];
+	free (ptr);
+	return (nptr);
+}
