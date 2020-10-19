e01e8be777d971127177954fbc01244c7bd0ba5d
Diego Lopez <dfelo93@gmail.com>
2020-10-18 23:03:14 -0500

    Prints sum of factors of 3 and 5 less than 1024.

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Prints sum of factors of 3 and 5 less than 1024.
+ */
+void main(void)
+{
+	int i, num = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			num+=i;
+	}
+	printf("%d\n", num);
+}
