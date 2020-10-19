f7f14a145111829043ee79404072910a4d793f72
Diego Lopez <dfelo93@gmail.com>
2020-10-19 14:36:28 -0500

    Added prototype for init_dog function in 1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/* Initialize a variable of type struct dog */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* __FILE__H_ */
