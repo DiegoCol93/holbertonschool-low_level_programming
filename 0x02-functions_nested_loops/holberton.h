f8fe074f8939da7971ac002aa62ac8f3544dd069
Diego Lopez <dfelo93@gmail.com>
2020-10-18 20:24:16 -0500

    Added prototype print_times_table

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,46 +1,46 @@
 #include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout.
  */
 int _putchar(char c);
 
 /**
- * print_alphabet - Prints alphabet from a to z
+ * print_alphabet - Prints alphabet from a to z.
  */
 void print_alphabet(void);
 
 /**
- * print_alphabet_x10 - Prints alphabet from a to z ten times
+ * print_alphabet_x10 - Prints alphabet from a to z ten times.
  */
 void print_alphabet_x10(void);
 
 /**
- * _islower - Checks if c is an upper or lower case character
+ * _islower - Checks if c is an upper or lower case character.
  */
 int _islower(int c);
 
 /**
- * _isalpha - Chekcs if c is an alphabetic character or not
+ * _isalpha - Chekcs if c is an alphabetic character or not.
  */
 int _isalpha(int c);
 
 /**
- * print_sign - Prints sign of number n
+ * print_sign - Prints sign of number n.
  */
 int print_sign(int n);
 
 /**
- * _abs - Computes the absolute value of a number
+ * _abs - Computes the absolute value of a number.
  */
 int _abs(int);
 
 /**
- * print_last_digit - Prints the last digit of a number
+ * print_last_digit - Prints the last digit of a number.
  */
 int print_last_digit(int);
 
 /**
- * jack_bauer - Prints minutes of a day
+ * jack_bauer - Prints minutes of a day.
  */
 void jack_bauer(void);
 
@@ -55,6 +55,11 @@ void times_table(void);
 int add(int, int);
 
 /**
- * print_to_98 - prints from input number to 98
+ * print_to_98 - prints from input number to 98.
  */
 void print_to_98(int n);
+
+/**
+ * print_times_table - Prints the n times table.
+ */
+void print_times_table(int n);
