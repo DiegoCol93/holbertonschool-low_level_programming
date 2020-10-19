226d677a31f520457de83e46764d64614a4ac71f
Diego Lopez <dfelo93@gmail.com>
2020-10-18 23:07:38 -0500

    Fixed betty style line 13 (space around +=)

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - Prints sum of factors of 3 and 5 less than 1024.
+ * natural - Prints sum of factors of 3 and 5 less than 1024.
  */
-void main(void)
+void natural(void)
 {
 	int i, num = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-			num+=i;
+			num += i;
 	}
 	printf("%d\n", num);
 }
