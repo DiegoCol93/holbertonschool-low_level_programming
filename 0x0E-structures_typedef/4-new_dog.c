956d2d3443bb8ff97741e9c844b2c211e20bfeb5
Diego Lopez <dfelo93@gmail.com>
2020-10-19 23:10:11 -0500

    inverted if condition for null return

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,33 +20,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	while (owner[lown])
 		lown++;
 	dog = malloc(sizeof(dog_t));
-	if (dog)
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(lname + 1);
+	if (dog->name)
 	{
-		dog->name = malloc(lname + 1);
-		if (dog->name)
-		{
-			for (i = 0; i <= lname + 1; i++)
-				dog->name[i] = name[i];
-		}
-		else
-		{
-			free(dog);
-			return (NULL);
-		}
-		dog->owner = malloc(lown + 1);
-		if (dog->owner)
-		{
-			for (i = 0; i <= lown + 1; i++)
-				dog->owner[i] = owner[i];
-		}
-		else
-		{
-			free(dog->name);
-			free(dog);
-			return (NULL);
-		}
-		dog->age = age;
-		return (dog);
+		for (i = 0; i <= lname + 1; i++)
+			dog->name[i] = name[i];
 	}
-	return (NULL);
+	else
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(lown + 1);
+	if (dog->owner)
+	{
+		for (i = 0; i <= lown + 1; i++)
+			dog->owner[i] = owner[i];
+	}
+	else
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->age = age;
+	return (dog);
 }
