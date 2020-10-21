5ec8886e4a30ab3a4b5a1c25940e95cfe1c353f1
Diego Lopez <dfelo93@gmail.com>
2020-10-21 12:09:05 -0500

    Fixed betty style, check if name NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,11 @@
 #include "function_pointers.h"
 /**
  * print_name - Function that prints a name.
+ * @name: String of the name to print.
+ * @f: Fucntion pointer to called function to print.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name)
+		(*f)(name);
 }
