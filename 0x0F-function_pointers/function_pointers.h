d4ab9c3c0b01ea4804aa9b5b36d77a58da05d947
Diego Lopez <dfelo93@gmail.com>
2020-10-21 13:43:57 -0500

    Added prototype array_iterator, and stdlib.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,14 @@
 #ifndef __FILE__H_
 #define __FILE__H_
+#include <stdlib.h>
 
 /* _putchar - writes the character c to stdout. */
 int _putchar(char c);
 
-/* print_name - function that prints a name. */
+/* Print_name - function that prints a name. */
 void print_name(char *name, void (*f)(char *));
+
+/* Executes a function given as a parameter on each element of an array. */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* __FILE__H_ */
