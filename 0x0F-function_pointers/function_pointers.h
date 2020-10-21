ec689ce78afce0b0d00430e7d32f2d83fc18d5ae
Diego Lopez <dfelo93@gmail.com>
2020-10-21 11:56:29 -0500

    Library of prototype functions.

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef __FILE__H_
+#define __FILE__H_
+
+/* _putchar - writes the character c to stdout. */
+int _putchar(char c);
+
+/* print_name - function that prints a name. */
+void print_name(char *name, void (*f)(char *));
+
+#endif /* __FILE__H_ */
