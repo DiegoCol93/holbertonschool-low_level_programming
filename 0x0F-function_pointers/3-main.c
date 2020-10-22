87c2bdc4a965ffc88af7ec6e245f644d7fa9e303
Diego Lopez <dfelo93@gmail.com>
2020-10-21 23:53:31 -0500

    Corrected if condition and atoi asignation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,19 +11,18 @@ int main(int argc, char **argv)
 {
 	int res, a, b;
 
-	a = (atoi(argv[1]));
-	b = (atoi(argv[3]));
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (b == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	if (argv[3][0] == '0' && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	a = (atoi(argv[1]));
+	b = (atoi(argv[3]));
 	res = get_op_func(argv[2])(a, b);
 	printf("%d\n", res);
 	return (0);
