9ba4e5e721157008b4b31f1fd4dd4b7725d44c51
Diego Lopez <dfelo93@gmail.com>
2020-10-21 23:39:09 -0500

    Stores operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - Adds a and b.
+ * @a: Number a.
+ * @b: Number b.
+ *
+ * Return: Operation's result.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Adds a and b.
+ * @a: Number a.
+ * @b: Number b.
+ *
+ * Return: Operation's result.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Multiplies a and b.
+ * @a: Number a.
+ * @b: Number b.
+ *
+ * Return: Operation's result.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divides a and b.
+ * @a: Number a.
+ * @b: Number b.
+ *
+ * Return: Operation's result.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Operates a modulus b.
+ * @a: Number a.
+ * @b: Number b.
+ *
+ * Return: Operation's result.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
