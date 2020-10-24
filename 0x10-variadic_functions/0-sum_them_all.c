d781a70924416b09893e7e55115f0a92cf4c5a0d
Diego Lopez <dfelo93@gmail.com>
2020-10-24 12:39:53 -0500

    F

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,11 +11,15 @@ int sum_them_all(const unsigned int n, ...)
 	int res = 0;
 	unsigned int i;
 
-	va_start(nums, n); /* Initializes arg list */
-	for (i = 0; i < n; i++)
+	if (n)
 	{
-		res += va_arg(nums, int);
+		va_start(nums, n); /* Initializes arg list */
+		for (i = 0; i < n; i++)
+		{
+			res += va_arg(nums, int);
+		}
+		va_end(nums);
+		return (res);
 	}
-	va_end(nums);
-	return (res);
+	return (0);
 }
