b06717d83d0ca01d0f502e8ba13be8ec9a4c5384
Diego Lopez <dfelo93@gmail.com>
2020-10-30 13:03:24 -0500

    Updated description of prototype 4. free_list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,10 +30,10 @@ size_t list_len(const list_t *h);
 /* 2. add_node - Adds a new node at the beginning of a list_t list. */
 list_t *add_node(list_t **head, const char *str);
 
-/* 3.  */
+/* 3. Adds a new node at the end of a list_t list. */
 list_t *add_node_end(list_t **head, const char *str);
 
-/* 4. */
+/* 4. Frees a list_t list. */
 void free_list(list_t *head);
 
 
