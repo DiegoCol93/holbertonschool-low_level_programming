719b852fa5661f8ec03f1b8fc18ff60068d1d4f3
Diego Lopez <dfelo93@gmail.com>
2020-11-04 11:07:44 -0500

    Returns pointer to an indicated node.

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - Returns pointer to an indicated node.
+ *
+ *  Arguments:
+ *    @head:  -    -    - Pointer to target head of the linked list.
+ *   @index:  -    -    - Position for the node to return.
+ *
+ *   Return:  -    -    - Pointer to the indicated node.
+ *
+ * |--------- Written by Diego Lopez, November 3rd 2020. ----------|
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head)
+	{
+		while (i < index)
+		{
+			head = head->next;
+			i++;
+		}
+	}
+	return (head);
+}
