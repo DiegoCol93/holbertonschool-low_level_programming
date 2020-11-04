c127346c37b764e2525533b2f989e9f12d1fee5a
Diego Lopez <dfelo93@gmail.com>
2020-11-04 14:28:55 -0500

    Fixed next position of new node.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 		{
 			new->n = n;
-			new->next = NULL;
+			new->next = *head;
 			pre->next = new;
 		}
 	}
