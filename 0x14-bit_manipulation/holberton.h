044c19ad4d60387837c71a987f3670c02947b761
Diego Lopez <dfelo93@gmail.com>
2020-11-05 14:49:46 -0500

    Custom library for current project.

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,10 @@
+#ifndef __FILE___H_
+#define __FILE___H_
+#include <stdio.h>
+
+/* 0. Converts a binary number to an unsigned int.*/
+unsigned int binary_to_uint(const char *b);
+
+/* 1. */
+
+#endif /* End of if __FILE___H_ exists */
