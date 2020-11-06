69c0c113917c9690821a36611a6e2c4aac1813ac
Diego Lopez <dfelo93@gmail.com>
2020-11-05 20:21:15 -0500

    Added prototype print_binary

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,10 +1,13 @@
 #ifndef __FILE___H_
 #define __FILE___H_
 #include <stdio.h>
+/* Writes the character c to stdout. */
+int _putchar(char c);
 
 /* 0. Converts a binary number to an unsigned int.*/
 unsigned int binary_to_uint(const char *b);
 
-/* 1. */
+/* 1. Prints the binary form of a positive int number. */
+void print_binary(unsigned long int n);
 
 #endif /* End of if __FILE___H_ exists */
