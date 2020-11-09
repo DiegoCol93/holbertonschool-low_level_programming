caf4f60fd1525753926f9f112a85f69210f8b23b
Diego Lopez <dfelo93@gmail.com>
2020-11-09 12:50:17 -0500

    Moved if when text_content NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,12 +15,12 @@ int create_file(const char *filename, char *text_content)
 	unsigned int len = 0;
 	int fd, check = 0;
 
+	if (!text_content)
+		text_content = "";
 	for (len = 0; text_content[len]; len++)
 	{}
 	if (filename)
 	{
-		if (!text_content)
-			text_content = "";
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (fd == -1)
 			return (-1);
