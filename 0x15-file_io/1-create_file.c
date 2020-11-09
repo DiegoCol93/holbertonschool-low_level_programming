c1b55795f94259c9ddeec48d395c537ba903e31f
Diego Lopez <dfelo93@gmail.com>
2020-11-09 12:28:09 -0500

    Function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * create_file     - Function that creates a file.
+ *
+ *   Arguments:
+ * @text_content:  - Contents to add into file.
+ *   @filename:    - Name of file to create.
+ *
+ *    Return:      - 1 On succes, (-1) on failure.
+ *
+ * |- Written by Diego Lopez - November/09/2020. -|
+ */
+int create_file(const char *filename, char *text_content)
+{
+	unsigned int len = 0;
+	int fd, check = 0;
+
+	for (len = 0; text_content[len]; len++)
+	{}
+	if (filename)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		check = write(fd, text_content, len);
+		if (check == -1)
+		{
+			return (-1);
+		}
+		close(fd);
+	}
+	return (-1);
+}
