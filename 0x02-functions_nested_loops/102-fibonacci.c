a770380140fa991e31afc0e4df6924cd26280bf6
Diego Lopez <dfelo93@gmail.com>
2020-11-29 23:24:32 -0500

    Prints up to 20365011074

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int64_t a = 1, b = 2, c;
 
-	while (a < 100000000 && b < 100000000 && c < 100000000)
+	while (a < 100000000000 && b < 100000000000 && c < 100000000000)
 	{
 		c = a + b;
 		printf("%lu", a);
 		printf(", ");
 
 		printf("%lu", b);
-		if (b >= 63245986)
+		if (b >= 20365011074)
 			break;
 		printf(", ");
 
