1653f82fa292426fa55f52e611700d276ef08edb
Diego Lopez <dfelo93@gmail.com>
2020-11-29 23:59:33 -0500

    Prints sum of Odds less than 4million

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,37 +1,24 @@
 #include <stdio.h>
+#include <stdint.h>
 /**
- * main - Prints the first 10 Odd Fibonacci numbers.
+ * main - Prints the sum of the Odd numbers less tha 4 million
+ *        in the fibonacci sequence.
  *
  * Return: 0 On success.
  */
 int main(void)
 {
-	unsigned int a = 1, b = 2, c;
+	int64_t a = 1, b = 2, c = 0;
+	int64_t res = 0;
 
-	while (a < 1000000 && b < 1000000 && c < 1000000)
+	while (a < 4000000 && b < 4000000 && c < 4000000)
 	{
 		c = a + b;
-
-		if (a % 2 == 0)
-		{
-			printf("%u", a);
-			printf(", ");
-		}
-		if (b % 2 == 0)
-		{
-			printf("%u", b);
-			if (b >= 832040)
-				break;
-			printf(", ");
-		}
-		if (c % 2 == 0)
-		{
-			printf("%u", c);
-			printf(", ");
-		}
+		res = res + b;
 		a = b + c;
 		b = c + a;
 	}
+	printf("%lu", res);
 	putchar(10);
 	return (0);
 }
