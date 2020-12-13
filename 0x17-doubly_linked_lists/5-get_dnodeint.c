32cb18630f18b3465c2d10c39e7830d997f28672
Diego Lopez <dfelo93@gmail.com>
2020-12-13 10:11:07 -0500

    Added proper commenting and betty style.

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 /**
+ * get_dnodeint_at_index - Returns a node at the given index, from a
+ *                         doubly linked list.
+ *       Arguments:
+ *         @head:        - Pointer to head of linked list.
+ *        @index:        - Number of node to get.
  *
+ *        Return:        - Node at given index, or
+ *                         NULL if no node exists or failed to get it.
+ *              _____   __  ______  ______  ______
+ *             /\  __-./\ \/\  ___\/\  ___\/\  __ \
+ *             \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
+ *              \ \____-\ \_\ \_____\ \_____\ \_____\
+ *               \/____/ \/_/\/_____/\/_____/\/_____/
+ *              __      ______  ______  ______  ______
+ *             /\ \    /\  __ \/\  == \/\  ___\/\___  \
+ *             \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
+ *              \ \_____\ \_____\ \_\   \ \_____\/\_____\
+ *               \/_____/\/_____/\/_/    \/_____/\/_____/
+ *
+ *                              Dec-2020
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
@@ -13,9 +32,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		for (i = 0; i < index; i++)
 		{
 			if (!get_node)
-				return(NULL);
+				return (NULL);
 			get_node = get_node->next;
 		}
 	}
-	return(get_node);
+	return (get_node);
 }
