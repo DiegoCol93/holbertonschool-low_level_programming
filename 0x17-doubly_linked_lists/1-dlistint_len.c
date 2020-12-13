211dc4b106f607d801636551798a407cc38127fc
Diego Lopez <dfelo93@gmail.com>
2020-12-13 01:20:46 -0500

    Proper commenting and betty style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 /**
+ * dlistint_len - Returns number of nodes of a
+ *                doubly linked list.
  *
+ *   Arguments:
+ *      @h:     - Pointer to head of the list.
+ *
+ *    Return:   - Number of nodes.
+ *      _____   __  ______  ______  ______
+ *     /\  __-./\ \/\  ___\/\  ___\/\  __ \
+ *     \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
+ *      \ \____-\ \_\ \_____\ \_____\ \_____\
+ *       \/____/ \/_/\/_____/\/_____/\/_____/
+ *      __      ______  ______  ______  ______
+ *     /\ \    /\  __ \/\  == \/\  ___\/\___  \
+ *     \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
+ *      \ \_____\ \_____\ \_\   \ \_____\/\_____\
+ *       \/_____/\/_____/\/_/    \/_____/\/_____/
+ *
+ *                     Dec-2020
  */
 size_t dlistint_len(const dlistint_t *h)
 {
@@ -10,7 +28,7 @@ size_t dlistint_len(const dlistint_t *h)
 	if (h)
 	{
 		current = h;
-		while(current)
+		while (current)
 		{
 			nodes++;
 			current = current->next;
