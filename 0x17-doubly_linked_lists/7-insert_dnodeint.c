7b955296dfc7b8499ca7fbddd2185eb4c0fbb49b
Diego Lopez <dfelo93@gmail.com>
2020-12-13 13:32:12 -0500

    Condition for next node properly placed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,11 +42,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				past = past->next;
 			if (!past)
 				return (NULL);
-			if (past->next)
-				past->next->prev = new;
 			new->next = past->next;
 			new->prev = past;
-			past->next->prev = new;
+			if (past->next)
+				past->next->prev = new;
 			past->next = new;
 		}
 		else
