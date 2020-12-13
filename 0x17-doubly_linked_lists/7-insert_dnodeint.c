9127d0d97d56bd52d4012317880c2b5010efa491
Diego Lopez <dfelo93@gmail.com>
2020-12-13 13:38:55 -0500

    Added and past node exists.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = NULL;
 		new->prev = NULL;
 		past = *h;
-		if (idx > 0)
+		if (idx > 0 && past)
 		{
 			for (; idx > 1 && past; idx--)
 				past = past->next;
