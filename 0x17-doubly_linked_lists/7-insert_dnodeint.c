b619459bb98236066a81fecf1945ff27556fcc97
Diego Lopez <dfelo93@gmail.com>
2020-12-13 11:32:40 -0500

    Properly fixed case for node at end, removed tag to previous commit, sorry @end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -60,6 +60,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			current->next = new_node;
 		}
 		current->prev = new_node; /* Set new_node's prev. */
+
 	}
 	return (new_node);
 }
