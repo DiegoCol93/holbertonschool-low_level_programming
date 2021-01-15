17602cfd04b88ceba27c48e6e81fb82cfedfc445
Diego Lopez <dfelo93@gmail.com>
2021-01-15 08:58:40 -0500

    Check for NULL value.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,8 +26,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *tmp = NULL;
 	unsigned long int index = 0;
 
-	/* If empty or NULL key or NULL table. */
-	if (!strcmp(key, "") || !key || !ht)
+	/* If empty or NULL key or NULL table. or NULL value */
+	if (!strcmp(key, "") || !key || !ht || !value)
 		return (1);
 	/* Get index using hash function. */
 	index = key_index((unsigned char *)key, ht->size);
