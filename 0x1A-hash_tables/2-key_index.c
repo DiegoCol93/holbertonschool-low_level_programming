d83cd3fb13303e7dacd234fe8ea2c2eabcf4a633
Diego Lopez <dfelo93@gmail.com>
2021-01-16 01:06:49 -0500

    Calculates the index for the given key.

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,18 +1,25 @@
 #include "hash_tables.h"
-
 /**
- * key_index - Function to get the index.
+ * key_index  - Calculates the corresponding hash index, for the size of table.
  *
- * @key: Key value or pre hash value.
- * @size: Size of the hash table, total number of index.
+ * Arguments:
+ *    @key:   - Pointer to key string to calculate the index from.
+ *   @size:   - Size of the hash table.
  *
- * Return: Index number for the hash table
+ *  Return:   - The corresponding index for the given key.
+ *         _____   __  ______  ______  ______
+ *        /\  __-./\ \/\  ___\/\  ___\/\  __ \
+ *        \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
+ *         \ \____-\ \_\ \_____\ \_____\ \_____\
+ *          \/____/ \/_/\/_____/\/_____/\/_____/
+ *         __      ______  ______  ______  ______
+ *        /\ \    /\  __ \/\  == \/\  ___\/\___  \
+ *        \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
+ *         \ \_____\ \_____\ \_\   \ \_____\/\_____\
+ *          \/_____/\/_____/\/_/    \/_____/\/_____/
+ *                                      Jan - 2021
  */
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = 0;
-
-	index = hash_djb2(key) % size;
-	return (index);
+	return (hash_djb2(key) % size);
 }
