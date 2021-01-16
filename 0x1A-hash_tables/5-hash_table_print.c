9bbc19a3f698ba82c0e00d4eb9b07656e823724e
Diego Lopez <dfelo93@gmail.com>
2021-01-15 23:06:19 -0500

    Rework.

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,88 +1,37 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_print - Prints the contents of a hash table.
+ * hash_table_print - Function to print all values of the hash table.
+ * @ht: Pointer to the hash table created.
  *
- *     Argument:
- *        @ht:      - Pointer to the hash node table.
- *           _____   __  ______  ______  ______
- *          /\  __-./\ \/\  ___\/\  ___\/\  __ \
- *          \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
- *           \ \____-\ \_\ \_____\ \_____\ \_____\
- *            \/____/ \/_/\/_____/\/_____/\/_____/
- *           __      ______  ______  ______  ______
- *          /\ \    /\  __ \/\  == \/\  ___\/\___  \
- *          \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
- *           \ \_____\ \_____\ \_\   \ \_____\/\_____\
- *            \/_____/\/_____/\/_/    \/_____/\/_____/
- *                                       Jan - 2021
+ * Return: Always nothing.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int number_of_keys = 0;
-	unsigned long int index = 0;
 	hash_node_t *tmp = NULL;
+	unsigned long int i = 0;
+	int sw = 0;
 
-	if (!ht)
+	if (ht == NULL)
 	{
-		printf("{}\n");
-		exit(1);
+		return;
 	}
-	number_of_keys = count_keys(ht);
-	putchar('{');
-	for (; index < ht->size; index++)
+	printf("{");
+	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[index])
+		if (ht->array[i] == NULL)
+			continue;
+		if (sw == 1)
+			printf(", ");
+		sw = 1;
+		tmp = ht->array[i];
+		while (tmp)
 		{
-			tmp = ht->array[index];
-			while (tmp)
-			{
-				printf("'%s': '%s'", tmp->key, tmp->value);
-				if (number_of_keys > 1)
-					printf(", ");
-				number_of_keys -= 1;
-				tmp = tmp->next;
-			}
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			if (tmp->next != NULL)
+				printf(", ");
+			tmp = tmp->next;
 		}
 	}
-	putchar('}');
-	putchar('\n');
-}
-
-/**
- * count_keys - Counts the number of keys on a hash table.
- *
- *  Argument:
- *     @ht:   - Pointer to the hash node table.
- *
- *   Return:  - Number of keys present on the hash table.
- *           _____   __  ______  ______  ______
- *          /\  __-./\ \/\  ___\/\  ___\/\  __ \
- *          \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
- *           \ \____-\ \_\ \_____\ \_____\ \_____\
- *            \/____/ \/_/\/_____/\/_____/\/_____/
- *           __      ______  ______  ______  ______
- *          /\ \    /\  __ \/\  == \/\  ___\/\___  \
- *          \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
- *           \ \_____\ \_____\ \_\   \ \_____\/\_____\
- *            \/_____/\/_____/\/_/    \/_____/\/_____/
- *                                       Jan - 2021
- */
-unsigned long int count_keys(const hash_table_t *ht)
-{
-	unsigned long int number = 0, index = 0;
-	hash_node_t *tmp = NULL;
-
-	tmp = ht->array[index];
-
-	for (; index < ht->size; index++)
-		if (ht->array[index])
-		{
-			tmp = ht->array[index];
-			while (tmp)
-			{
-				number += 1;
-				tmp = tmp->next;
-			}
-		}
-	return (number);
+	printf("}\n");
 }
