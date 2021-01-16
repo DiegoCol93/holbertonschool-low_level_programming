e01db9619a1faf108af1e2373f8469eb3c490732
Diego Lopez <dfelo93@gmail.com>
2021-01-15 23:43:36 -0500

    Proper return.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,64 +1,45 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - Sets a key:value pair on the table.
+ * hash_table_set - Function that adds an element to the hash table.
+ * @ht: Pointer to the hash table created.
+ * @key: Key value or string to save.
+ * @value: Value associated with the key.
  *
- *    Arguments:
- *       @ht:     - Pointer to the hash table.
- *      @key:     - Pointer to key string.
- *    @value:     - Pointer to value string.
- *
- *     Return:    - 0 If succesful, 1 if
- *                  something ocurred.
- *         _____   __  ______  ______  ______
- *        /\  __-./\ \/\  ___\/\  ___\/\  __ \
- *        \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
- *         \ \____-\ \_\ \_____\ \_____\ \_____\
- *          \/____/ \/_/\/_____/\/_____/\/_____/
- *         __      ______  ______  ______  ______
- *        /\ \    /\  __ \/\  == \/\  ___\/\___  \
- *        \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
- *         \ \_____\ \_____\ \_\   \ \_____\/\_____\
- *          \/_____/\/_____/\/_/    \/_____/\/_____/
- *                                      Jan - 2021
+ * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = NULL, *tmp = NULL;
 	unsigned long int index = 0;
+	unsigned long int size = 0;
+	hash_node_t *new_node = NULL;
+	hash_node_t *tmp =  NULL;
 
-	/* If empty or NULL key or NULL table. or NULL value */
-	if (strcmp(key, "") == 0 || !key || !ht || !value)
+	if (ht == NULL || key == NULL)
 		return (0);
-	/* Get index using hash function. */
-	index = key_index((unsigned char *)key, ht->size);
-
-	/* If there's a node in the current index. */
-	if (ht->array[index])
-	{       /* Set tmp pointer. */
-		tmp = ht->array[index];
-		while (tmp)
-		{       /* While tmp exists. */
-			if (strcmp(tmp->key, key) == 0) /* If == keys. */
-			{
-				/* Set new value. */
-				tmp->value = strdup(value);
-				return (1);
-			}
-			/* Move the tmp pointer. */
-			tmp = tmp->next;
+	size = ht->size;
+	index = key_index((unsigned char *)key, size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (tmp->value == NULL)
+				return (0);
+			return (1);
 		}
+		tmp = tmp->next;
 	}
-	/* Allocate memory for new new_node. */
 	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	if (new_node == NULL)
 		return (0);
-	/* Set the key for the new_node. */
 	new_node->key = strdup(key);
-	/* Set the value data for the new node. */
 	new_node->value = strdup(value);
-	/* Set the new_node' next to NULL. */
-	new_node->next = ht->array[index];
-	/* Set "head" pointer. */
+	if (new_node->value == NULL)
+		return (0);
+	new_node->next =  ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
 }
