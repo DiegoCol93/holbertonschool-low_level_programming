4571414ffe9996a9fbccbb10132cc87051ee5f24
Diego Lopez <dfelo93@gmail.com>
2021-01-16 01:03:56 -0500

    Creates a new hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,20 +1,45 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - function that returns a hash table pointer
- * @size: Size of the array that contains the hash.
+ * hash_table_create - Creates a hash table.
  *
- * Return: Pointer to the hash table.
+ *    Arguments:
+ *      @size:      - Size of the hash table to create.
+ *
+ *     Return:      - The newly created table, or NULL
+ *                    if not able to allocate.
+ *         _____   __  ______  ______  ______
+ *        /\  __-./\ \/\  ___\/\  ___\/\  __ \
+ *        \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
+ *         \ \____-\ \_\ \_____\ \_____\ \_____\
+ *          \/____/ \/_/\/_____/\/_____/\/_____/
+ *         __      ______  ______  ______  ______
+ *        /\ \    /\  __ \/\  == \/\  ___\/\___  \
+ *        \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
+ *         \ \_____\ \_____\ \_\   \ \_____\/\_____\
+ *          \/_____/\/_____/\/_/    \/_____/\/_____/
+ *                                      Jan - 2021
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *h_table = NULL;
+	hash_table_t *table = NULL;
+	unsigned long int index = 0;
 
-	h_table = malloc(sizeof(hash_table_t));
-	if (h_table == NULL)
+	/* Allocates table struct. */
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
 		return (NULL);
-	h_table->size = size;
-	h_table->array = calloc(h_table->size, sizeof(hash_node_t *));
-	if (h_table->array == NULL)
+
+	/* Sets size of the table. */
+	table->size = size;
+
+	/* Allocates the number of head pointers. */
+	table->array = malloc(size * sizeof(hash_node_t *));
+	if (!table->array)
 		return (NULL);
-	return (h_table);
+
+	/* Initialize to NULL each cell. */
+	for (; index < size; index++)
+		table->array[index] = NULL;
+
+	return (table);
 }
