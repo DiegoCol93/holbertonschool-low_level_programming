d399183a15739c2bec6661f0dcd2f61d80748472
Diego Lopez <dfelo93@gmail.com>
2021-01-16 01:09:54 -0500

    Gets the value for the given key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,26 +1,39 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_get - Function that retrieves a value associated with a key.
- * @ht: Pointer to the hash table created.
- * @key: Key value or string to save.
+ * hash_table_get - Gets the value from the key in the dictionary.
  *
+ *   Arguments:
+ *      @ht:      - Pointer to the hash table.
+ *     @key:      - Pointer to string of the key.
  *
- * Return: Value associated with the element, or NULL if key couldnt be found.
+ *    Return:     - The pointer to the string of the corresponding
+ *                   value or NULL if no key exist or no key is
+ *		     given or no hash table exists.
+ *              _____   __  ______  ______  ______
+ *             /\  __-./\ \/\  ___\/\  ___\/\  __ \
+ *             \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
+ *              \ \____-\ \_\ \_____\ \_____\ \_____\
+ *               \/____/ \/_/\/_____/\/_____/\/_____/
+ *              __      ______  ______  ______  ______
+ *             /\ \    /\  __ \/\  == \/\  ___\/\___  \
+ *             \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
+ *              \ \_____\ \_____\ \_\   \ \_____\/\_____\
+ *               \/_____/\/_____/\/_/    \/_____/\/_____/
+ *                                          Jan - 2021
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
-	unsigned long int size = 0;
 	hash_node_t *tmp = NULL;
 
-	if (ht != NULL && key != NULL)
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (ht->array[index])
 	{
-		size =  ht->size;
-		index = key_index((unsigned char *)key, size);
 		tmp = ht->array[index];
-		if (tmp == NULL)
-			return (NULL);
 		while (tmp)
 		{
 			if (strcmp(tmp->key, key) == 0)
