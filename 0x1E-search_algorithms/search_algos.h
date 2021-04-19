dbde3dba41b4018bc2e6d25fe8a75a3bb004d3f1
DiegoCol93 <dfelo93@gmail.com>
2021-04-19 03:15:43 -0500

    |--ADDED--| Prototype for 100-jump_search.

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,8 @@ int linear_search(int *array, size_t size, int value);
 /* 1. Binary search | Binary searches for an int number. - - - - - - - - - - */
 int binary_search(int *array, size_t size, int value);
 
+/* 100. Jump search | Jumps searching for an int number. - - - - - - - - - - */
+int jump_search(int *array, size_t size, int value);
+
+
 #endif /* _SEARCH_ALGOS_H_ */
