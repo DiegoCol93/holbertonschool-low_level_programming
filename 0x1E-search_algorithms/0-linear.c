dad1e6901d101074974b7c451c3d1414e002d473
DiegoCol93 <dfelo93@gmail.com>
2021-04-20 14:41:48 -0500

    |- FIXED -| Extra semicolon.

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,7 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t index = 0;
 
 	if (array) /* Is not NULL */
-		for (index = 0; index < size; array++, index++;)
+		for (index = 0; index < size; array++, index++)
 		{
 			printf("Value checked array[%lu] = [%d]\n", index, *array);
 			if (value == *array)
